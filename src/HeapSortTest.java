a71f6aef759509b636fb43ed89ef5f159e590298
charliekelley21 <charlk21@vt.edu>
2020-08-04 17:56:09 -0400

    init of Project 3 project folder

---
+++ src/HeapSortTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import student.TestCase;
+
+public class HeapSortTest extends TestCase {
+
+    /**
+     * An artificial test to get initial coverage for the
+     * main method. Delete or modify this test.
+     */
+    @Test
+    public void testMain() {
+        HeapSort dum = new HeapSort();
+        assertNotNull(dum);
+        HeapSort.main(new String[3]);
+        assertEquals(systemOut().getHistory(), ""); // check that nothing was printed out
+    }
+
+}
