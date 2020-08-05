d21215c4814fcc048f551adfc9d8e1e45f767007
barakfinnegan <BJFinn98@vt.edu>
2020-08-05 19:20:50 -0400

    Merging with local

--- src/MinHeap.java
+++ src/MinHeap.java
@@ -96,4 +96,8 @@ public class MinHeap {
     public int[] getArray() {
         return heap;
     }
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> branch 'master' of https://web-cat.cs.vt.edu/Web-CAT/WebObjects/Web-CAT.woa/git/StudentProject/49247e6a-e557-46af-ac42-42beae785f8e
--- src/Record.java
+++ src/Record.java
@@ -1,10 +1,13 @@
 
 public class Record {
+<<<<<<< HEAD
     private short key;
     private short value;
     
     public short getKey() {
         return key;
     }
+=======
+>>>>>>> branch 'master' of https://web-cat.cs.vt.edu/Web-CAT/WebObjects/Web-CAT.woa/git/StudentProject/49247e6a-e557-46af-ac42-42beae785f8e
 
 }
