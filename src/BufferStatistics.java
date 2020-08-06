62fdb3eb71749e65302a4d7575d71b7e2c198b31
charliekelley21 <charlk21@vt.edu>
2020-08-06 14:49:24 -0400

    Finished Statistics class and tested

--- src/BufferStatistics.java
+++ src/BufferStatistics.java
@@ -1,4 +1,76 @@
 
+/**
+ * Controls the run-time statistics for the heapsort algorithm
+ * 
+ * @author Charlie Kelley (charlk21)
+ * @version 2020.08.06
+ */
 public class BufferStatistics {
+    // set up vars
+    private int cacheHits;
+    private int cacheMisses;
+    private int diskReads;
 
+    /**
+     * Default constructor BufferStatistics
+     */
+    public BufferStatistics() {
+        cacheHits = 0;
+        cacheMisses = 0;
+        diskReads = 0;
+    }
+
+
+    /**
+     * Increase the cache hit count
+     */
+    public void hit() {
+        cacheHits++;
+    }
+
+
+    /**
+     * Getter for the number of cache hits
+     * 
+     * @return cache hits
+     */
+    public int getHits() {
+        return cacheHits;
+    }
+
+
+    /**
+     * Increase the cache miss count
+     */
+    public void miss() {
+        cacheMisses++;
+    }
+
+
+    /**
+     * getter for the number of cache misses
+     * 
+     * @return cache misses
+     */
+    public int getMisses() {
+        return cacheMisses;
+    }
+
+
+    /**
+     * Increase the disk read count
+     */
+    public void diskRead() {
+        diskReads++;
+    }
+
+
+    /**
+     * getter for the number of disk reads
+     * 
+     * @return disk reads
+     */
+    public int getDiskReads() {
+        return diskReads;
+    }
 }
--- src/BufferStatisticsTest.java
+++ src/BufferStatisticsTest.java
@@ -1,4 +1,60 @@
+import student.TestCase;
 
-public class BufferStatisticsTest {
+/**
+ * Testing class for the BufferStatistics class
+ * 
+ * @author Charlie Kelley (charlk21)
+ * @version 2020.08.06
+ *
+ */
+public class BufferStatisticsTest extends TestCase {
 
+ // Setting up vars
+    private BufferStatistics test;
+
+    /**
+     * This is the setUp method for the testing suite.
+     */
+    public void setUp() {
+        test = new BufferStatistics();
+    }
+    
+    /**
+     * This will test the constructor for BufferStatistics
+     */
+    public void testConstructor() {
+        assertEquals(test.getHits(), 0);
+        assertEquals(test.getMisses(), 0);
+        assertEquals(test.getDiskReads(), 0);
+    }
+    
+    /**
+     * Test the Hits getter and setter
+     */
+    public void testHits() {
+        for (int i = 1; i <= 10; i++) {
+            test.hit();
+            assertEquals(test.getHits(), i);
+        }
+    }
+    
+    /**
+     * Test the Misses getter and setter
+     */
+    public void testMisses() {
+        for (int i = 1; i <= 10; i++) {
+            test.miss();
+            assertEquals(test.getMisses(), i);
+        }
+    }
+    
+    /**
+     * Test the Misses getter and setter
+     */
+    public void testDiskReads() {
+        for (int i = 1; i <= 10; i++) {
+            test.diskRead();
+            assertEquals(test.getDiskReads(), i);
+        }
+    }
 }
